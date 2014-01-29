cf4f928bb1f56c9cf51a63cd5a59ff56bcca2682
tussle <gaming124@gmail.com>
2014-01-29 23:49:41 +0000

    a basic start on the python, I kinda need the directory structure in place and the html templates etc

---
+++ python/genIndex.py
@@ -0,0 +1,7 @@
+#!usr\bin python
+import sys 
+import os
+#this program takes command line arguments
+#it expects the first argument to select a html template(stored @path)and subsequent inputs to be css styles, applied in order to css stylesheet template
+#attached to the html template
+path="/" +sys.args[1]
--- python/genNewSubDir.py
+++ python/genNewSubDir.py
@@ -1,1 +1,13 @@
 #!usr/bin python
+#a simple script for createing user subdirectory to act as physical website location.
+import os
+import sys
+path="" #this needs to be defined so the code currently won't run
+cont = True
+try:
+	os.mkdir(path+sys.argv[1])
+except OSError:
+	cont=False
+	print("oops we have experienced a problem") #more helpful error report is probably needed
+
+
